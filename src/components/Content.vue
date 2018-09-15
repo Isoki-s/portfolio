7eccb759c9f8e0b112956f274e005321f11e69c3
Satoru Isoki <isokisatoru@aol.com>
2018-09-15 16:48:20 +0900

    view

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,17 +1,45 @@
 <template lang="pug">
-v-container(fluid fill-height)
-
+  v-app(id="inspire")
+    v-container(fluid)
+      v-layout(row xs12 wrap)
+        v-flex(xs6 sm4 md3 v-for="list in lists" :key="list.id" class="skill_box")
+          p {{list.name}}
+          v-progress-circular(
+            :rotate="-90"
+            :size="150"
+            :width="20"
+            :value="list.level"
+            :color="list.color"
+          ) {{ list.level }}
+      
 </template>
 
 <script>
+// import axios from 'axios';
 export default {
-    data () {
-      return {
-        
+  data () {
+    return {
+      interval: {},
+      value: 0,
+      list: []
     }
+  },
+  mounted () {
+    this.interval = setInterval(() => {
+        if (this.value2 === 100) {
+          return false
+        }
+        this.value2 += 10
+      }, 1000)
+  },
+  created: function () {
+    var lists = require("../assets/skills.json")
+    this.lists = lists
   }
 }
 </script>
 
 <style lang="stylus" conputed>
+.skill_box
+  text-align center
 </style>

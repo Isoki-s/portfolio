9abadea60cab5536309e88d07ce21fbe863d0dd3
Satoru Isoki <isokisatoru@aol.com>
2018-10-12 00:03:07 +0900

    off header

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,32 +1,17 @@
 <template lang="pug">
-  header
-    v-navigation-drawer(
-      v-model="drawer"
-      fixed
-      app
-      )
-      v-list(dense)
-        v-list-tile(@click="")
-          v-list-tile-action
-            v-icon home
-          v-list-tile-content
-            v-list-tile-title Home
-        v-list-tile(@click="")
-          v-list-tile-action
-            v-icon contact_mail
-          v-list-tile-content
-            v-list-tile-title Contact
-    v-toolbar(color="indigo" dark fixed app)
-      v-toolbar-side-icon(@click.stop="drawer = !drawer")
-      v-toolbar-title Application
+  //- header
+  //-   v-toolbar(
+  //-     color="indigo"
+  //-     dark)
+  //-     v-toolbar-title Application
 </template>
 
 <script>
 export default {
   data () {
     return {
-      title: 'portfolio',
-      drawer: false
+      // title: 'portfolio',
+      // drawer: false
     }
   }
 }

a613de55c269a0212644e4d5fe9e918c99e41854
Satoru Isoki <isokisatoru@aol.com>
2018-10-12 01:50:11 +0900

    card

--- src/components/Made.vue
+++ src/components/Made.vue
@@ -1,14 +1,35 @@
 <template lang="pug">
   v-app(id="inspire")
     v-container(fluid)
-      v-layout(row xs12 wrap)
-        h1 made
+      v-layout(row xs12 wrap sm6 offset-sm3)
+        v-card
+          img(
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="200px"
+          )
+          v-card-title(primary-title)
+            div
+              .headline Slack Reminder
+              span(class="grey--text") Who What When
+          v-card-actions
+            v-btn(flat href="https://isoki-s.github.io/slack_reminder/" target="_blank") Try
+            v-spacer
+            v-btn(icon @click="show = !show")
+              v-icon {{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}
+          v-slide-y-transition
+            v-card-text(v-show="show") 
+                | Vue.js
+                br
+                | Pug
+                br
+                | Stylus
 </template>
 
 <script>
 export default {
   data () {
     return {
+      show: false
     }
   }
 }

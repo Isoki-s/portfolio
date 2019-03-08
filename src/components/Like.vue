f29caf203145e121c9ee80be49f3fa0c683fd2ad
Satoru Isoki <isokisatoru@aol.com>
2019-03-08 17:57:24 +0900

    like

--- src/components/Like.vue
+++ src/components/Like.vue
@@ -1,7 +1,17 @@
 <template lang="pug">
   v-container(fluid)
     v-layout(row xs12 wrap)
-      h1 Like
+      v-flex(xs12)
+        h2 Like
+    v-layout(row xs12 wrap)
+      v-flex(xs12)
+        p Travel / Eat / Movie / Music
+          | <br>Musical
+          | <br>Netflix / AmazonPrime / Hulu
+          | <br>Dog(especially Shiba) / Cat
+          | <br>Coffee
+          | <br>Apple / Gopro
+
 </template>
 
 <script>
@@ -14,4 +24,7 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+h2
+  text-align center
+  margin-bottom 20px
 </style>
